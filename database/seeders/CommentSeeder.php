0bd5f9b5e688503a1505633af85f2045f80ff1d9
Jose Rodriguez <jonate@nexura.com>
2022-05-18 21:10:53 -0500

    first commit

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('comments')->insert([
+            'post_id' => 1,
+            'reply' => 'repli'
+        ]);
+    }
+}
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'author_id' => 3,
+            'title' => 'Titulo tres',
+            'content' =>'esto es una prueba',
+        ]);
+    }
+}
